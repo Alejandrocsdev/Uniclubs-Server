7d40a0aee068a6a7ab9285d449210567afb4d5a7
Alejandrocsdev <alejandrocsdev@gmail.com>
2025-07-20 23:47:29 +0800

    update .env.sample

--- utils/url.js
+++ utils/url.js
@@ -1,13 +1,11 @@
 // Environment Variables
-const { NODE_ENV, SERVER_URL, PORT, CLIENT_URL, CLIENT_PORT, WIFI, WIFI_URL, PREVIEW, PREVIEW_PORT } = process.env
+const { NODE_ENV, SERVER_URL, PORT, CLIENT_URL, CLIENT_PORT, WIFI, WIFI_URL } = process.env
 
 const isProduction = NODE_ENV === 'production'
 const isWifi = WIFI === 'true'
-const isPreview = PREVIEW === 'true'
 const localhost = isWifi ? WIFI_URL : 'localhost'
-const clientPort = isPreview ? PREVIEW_PORT : CLIENT_PORT
 
 const serverUrl = isProduction ? SERVER_URL : `http://${localhost}:${PORT}`
-const clientUrl = isProduction ? CLIENT_URL : `http://${localhost}:${clientPort}`
+const clientUrl = isProduction ? CLIENT_URL : `http://${localhost}:${CLIENT_PORT}`
 
 module.exports = { serverUrl, clientUrl }
